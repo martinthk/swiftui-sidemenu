469993c0568dd6284ae0f656c2dcd3f8536f47bb
Martin T <martintom.work@gmail.com>
2021-05-05 15:47:03 +0800

    Initial Commit

---
+++ SwiftUI-SideMenu/SwiftUI_SideMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SideMenuApp.swift
+//  SwiftUI-SideMenu
+//
+//  Created by Martin Tom on 5/5/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SideMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
